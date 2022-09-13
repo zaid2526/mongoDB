68dfa3efbfe2799ff51fd8b6be19acd7756608f3
zaid <mzr16894@gmail.com>
2022-09-13 16:44:21 +0530

    task 1 (createProduct api )

--- util/database.js
+++ util/database.js
@@ -1,8 +1,40 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'root', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+// const sequelize = new Sequelize('node-complete', 'root', 'root', {
+//   dialect: 'mysql',
+//   host: 'localhost'
+// });
+// module.exports = sequelize;
 
-module.exports = sequelize;
+
+
+
+
+
+const mongodb=require('mongodb');
+
+const MongoClient=mongodb.MongoClient;
+let _db;
+const mongoConnect=(callback)=>{
+  MongoClient.connect(
+    'mongodb+srv://Atlas_admin:admin@udemymongo.lotdnrm.mongodb.net/shop?retryWrites=true&w=majority'
+    )
+      .then(client=>{
+        console.log(client);
+        _db=client.db();
+        callback(client);
+      })
+      .catch(err=>{
+        console.log(err);
+      });
+}
+
+const getDb=()=>{
+  if(_db){
+    return _db;
+  }
+  throw "no database found"
+}
+
+exports.mongoConnect=mongoConnect;
+exports.getDb=getDb;
