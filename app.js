2dcd56b9403bf5a6d05a9ed4ce7c9463054071bc
zaid <mzr16894@gmail.com>
2022-09-13 18:58:08 +0530

    task2 (crud operation on admin page)

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.set('views', 'views');
 
 
 const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -32,6 +33,10 @@ app.use((req, res, next) => {
 });
 
 app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+
+
 app.use(errorController.get404);
 
 
--- models/product.js
+++ models/product.js
@@ -1,22 +1,67 @@
-
+const mongodb=require('mongodb');
+const { TIS620 } = require('mysql2/lib/constants/charsets');
 const getDb=require('../util/database').getDb;
 
 class Product{
-  constructor(title,price,imageUrl,description){
+  constructor(title,price,description,imageUrl, id){
     this.title=title;
     this.price=price;
     this.description=description;
     this.imageUrl=imageUrl;
+    this._id=id;
   }
+  //only insert........
+  // save(){
+  //   const db=getDb();
+  //   return db.collection('products')
+  //   .insertOne(this)
+  //   .then((result)=>{
+  //     console.log(">>>>>>>>>>>>>> inserted");
+  //   })
+  //   .catch(err=>{console.log(err);})
+  // }
 
+  //update and insert.....
   save(){
+    let db=getDb();
+    let dbOp;
+    if(this._id){
+      dbOp=db.collection('products').updateOne({_id: new mongodb.ObjectId(this._id)}, {$set: this})
+
+    }else{
+      dbOp=db.collection('products').insertOne(this);
+    }
+    return dbOp.then(result=>{
+      console.log(result);
+    }).catch(err=>{console.log(err);})
+  }
+  static fetchAll(){
     const db=getDb();
     return db.collection('products')
-    .insertOne(this)
-    .then((result)=>{
-      console.log(">>>>>>>>>>>>>> inserted");
-    })
-    .catch(err=>{console.log(err);})
+      .find()
+      .toArray()
+      .then(products=>{
+        // console.log(produccts);
+        return products;
+      }).catch(err=>{console.log(err);})
+  }
+  static findById(prodId){
+    const db=getDb();
+    return db.collection('products')
+      .find({_id:new mongodb.ObjectId(prodId)}).next()
+      .then(product=>{console.log(product); return product})
+      .catch(err=>{console.log(err);})
+  }
+
+  static deleteById(prodId){
+    const db=getDb();
+    return db.collection('products')
+      .deleteOne({_id:new mongodb.ObjectId(prodId)})
+      .then(()=>{
+        console.log('deleted');
+      })
+      .catch(err=>{console.log(err);})
+
   }
 }
 
