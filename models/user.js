caea4c864ab17b1ea7ea844306b3610c5f646d5d
zaid <mzr16894@gmail.com>
2022-09-14 07:55:14 +0530

    task3( user table)

--- models/user.js
+++ models/user.js
@@ -1,16 +1,44 @@
-const Sequelize = require('sequelize');
+const mongodb=require('mongodb')
+const getDb=require('../util/database').getDb;
 
-const sequelize = require('../util/database');
+class User{
+  constructor(name,email){
+    this.name=name;
+    this.email=email
+  }
+  save(){
+    const db=getDb();
+    return db.collection('users').insertOne(this).then(user=>{return user}).catch(err=>{console.log(err);})
+  }
+  static findById(userId){
+    const db=getDb();
+    return db.collection('users').find({_id: new mongodb.ObjectId(userId)}).next().then().catch(err=>{console.log(err);})
+  }
+}
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+module.exports=User
 
-module.exports = User;
+
+
+
+
+
+
+
+
+// const Sequelize = require('sequelize');
+// const sequelize = require('../util/database');
+// const User = sequelize.define('user', {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true
+//   },
+//   name: Sequelize.STRING,
+//   email: Sequelize.STRING
+// });
+// module.exports = User;
+
+
+
